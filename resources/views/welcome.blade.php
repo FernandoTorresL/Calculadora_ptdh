defb8d346b370d4e0aeba77066f96eeb2017c11d
Fernando Torres <fertorresmx@gmail.com>
2019-04-29 14:25:35 -0500

    Add first View, Request and Controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,10 +16,33 @@
     <div class="row">
         <form action="/calculadora/calcula" method="post">
             @csrf
+
+            @if ($errors->any())
+                @php
+                    //dd($errors);
+                @endphp
+
+                @foreach ($errors->get('nombre_patron') as $error)
+                    <div class="text-danger">{{ $error }}</div>
+                @endforeach
+
+                @foreach ($errors->get('dias_laborados') as $error)
+                    <div class="text-danger">{{ $error }}</div>
+                @endforeach
+
+                @foreach ($errors->get('sueldo_mensual') as $error)
+                    <div class="text-danger">{{ $error }}</div>
+                @endforeach
+            @endif
             <div class="form-group">
-                Nombre Patrón: <input type="text" name="nombre_patron" class="form-control" placeholder="Nombre del Patrón">
-                Días laborados con este patrón: <input type="text" name="dias_laborados" class="form-control" placeholder="Días laborados">
-                Sueldo mensual con este patrón: <input type="text" name="sueldo_mensual" class="form-control" placeholder="Sueldo mensual">
+                Nombre Patrón:
+                <input type="text" name="nombre_patron" class="form-control" placeholder="Nombre del Patrón">
+
+                Días laborados con este patrón:
+                <input type="text" name="dias_laborados" class="form-control" placeholder="Días laborados">
+
+                Sueldo mensual con este patrón:
+                <input type="text" name="sueldo_mensual" class="form-control" placeholder="Sueldo mensual">
             </div>
 
             <div class="input-group text-right">
