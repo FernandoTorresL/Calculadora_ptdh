29c491900a7ce7520b1a2fd07cdc2c369ccf3958
Fernando Torres <fertorresmx@gmail.com>
2019-05-07 14:05:40 -0500

    Add code to implement table 2

--- app/Http/Controllers/CalculadoraController.php
+++ app/Http/Controllers/CalculadoraController.php
@@ -11,23 +11,57 @@ class CalculadoraController extends Controller
     //public function calcula(CalculaCuotasRequest $request)
     public function calcula()
     {
-        $tmptabla1 =
-            array(
-                '1' =>
-                    array(
-                       'salario' =>
-                            array('178.57', '200', '300'),
-                       'v25vsmdf' =>
-                            array('11', '22', '33')
-            ), '2' =>
-                    array(
-                    'salario' =>
-                        array('a', 'b', 'c'),
-                    'v25vsmdf' =>
-                        array('111', '222', '333')
-            )
-        );
 
+        //Create empty arrays
+        $tablas_calculos1 = [];
+        $tablas_calculos2 = [];
+
+        //Fill arrays with more elements, one for each month of a year
+        for ($i = 0; $i <= 12; $i++) {
+            array_push($tablas_calculos1, [
+                'salario'   => [null, null, null],
+                'v25vsmdf'  => [null, null, null],
+                'art28_25'  => [null, null, null],
+                'cf'        => [null, null, null],
+                'exc_pat'   => [null, null, null],
+                'exc_ob'    => [null, null, null],
+                'pd_pat'    => [null, null, null],
+                'pd_ob'     => [null, null, null],
+                'gmp_pat'   => [null, null, null],
+                'gmp_ob'    => [null, null, null],
+                'rt'        => [null, null, null],
+                'iv_pat'    => [null, null, null],
+                'iv_ob'     => [null, null, null],
+                'gps'       => [null, null, null],
+                'cuota_pat' => [null, null, null],
+                'cuota_ob'  => [null, null, null],
+                'total_cop' => [null, null, null]
+            ]);
+
+            array_push($tablas_calculos2, [
+                'salario'   => [null, null, null],
+                'v25vsmdf'  => [null, null, null],
+                'v20vsmdf'  => [null, null, null],
+                'dias'  => [null, null, null],
+                'aus'  => [null, null, null],
+                'inc'  => [null, null, null],
+                'retiro'  => [null, null, null],
+                'cv_pat'        => [null, null, null],
+                'cv_ob'   => [null, null, null],
+                'suma_rcv'    => [null, null, null],
+                'aportacion'    => [null, null, null],
+                'tot_pat'     => [null, null, null],
+                'tot_ob'   => [null, null, null],
+                'tot_rcv_infonavit'    => [null, null, null]
+            ]);
+        }
+
+
+
+        //dd($tablas_calculos2);
+        //$tmptabla1['1']['exc_ob'][1] = 1978;
+        //$tmptabla1['2']['pd_pat'][2] = 2019;
+        //$tmptabla1['12']['total_cop'][1] = 205000;
         //dd($tmptabla1['2']);
         //dd($tmptabla1);
         //dd($tmptabla1['2']['v25vsmdf'][1]);
@@ -37,11 +71,10 @@ class CalculadoraController extends Controller
 
         //]);
 
-        //return "Llegó";
-
         $title_calc01 = 'Calculadora02';
         return view('calc.calc_resultados', [
-            'tmptabla1' => $tmptabla1,
+            'tablas_calculos1' => $tablas_calculos1,
+            'tablas_calculos2' => $tablas_calculos2,
             'title_calc01' => $title_calc01,
         ]);
     }
--- resources/views/calc/calc_resultados.blade.php
+++ resources/views/calc/calc_resultados.blade.php
@@ -12,7 +12,8 @@
     </div>
 
     <div>
-        <table class="table small">
+
+        <table class="table small table-active">
             <thead class="thead-dark">
             <tr>
                 <th scope="col">Tabla 1</th>
@@ -20,13 +21,10 @@
             </thead>
 
             <tbody>
-            @foreach ($tmptabla1 as $mes)
+            @foreach ($tablas_calculos1 as $mes)
                 <thead class="thead-dark">
                 <tr>
-                    <th scope="col">Mes: {{ key($tmptabla1) }}</th>
-                    @php
-                        next($tmptabla1);
-                    @endphp
+                    <th scope="col">Mes: {{ key($tablas_calculos1) }}</th>
                 </tr>
                 <tr>
                     <th scope="col">Concepto</th>
@@ -37,26 +35,61 @@
                 </thead>
                 @foreach ($mes as $concepto)
                         <tr>
-                        {{--<th scope="row">{{ key($mes) . '|' . $key . ':' . $value }}</th>--}}
-                            <th scope="row">{{ key($mes) }}</th>
+                            <th scope="row">Tabla 1 | Mes: {{ key($tablas_calculos1) . ' | ' . key($mes) }}</th>
                     @foreach ($concepto as $key => $value)
-{{--                        @php--}}
-{{--                            dd($mes);--}}
-{{--                            dd(key($mes));--}}
-{{--                        @endphp--}}
                             <td>{{ $value }}</td>
-
                     @endforeach
                         </tr>
-                    @php
-                        next($mes);
-                    @endphp
+                    @php next($mes); @endphp
+
                 @endforeach
+
+                @php next($tablas_calculos1); @endphp
+
             @endforeach
             </tbody>
         </table>
 
+    </div>
 
+    <div>
+
+        <table class="table small table-success">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">Tabla 2</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach ($tablas_calculos2 as $mes)
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Mes: {{ key($tablas_calculos2) }}</th>
+                </tr>
+                <tr>
+                    <th scope="col">Concepto</th>
+                    <th scope="col">Row 0</th>
+                    <th scope="col">Row 1</th>
+                    <th scope="col">Row 2 - Total</th>
+                </tr>
+                </thead>
+                @foreach ($mes as $concepto)
+                    <tr>
+                        <th scope="row">Tabla 2 | Mes: {{ key($tablas_calculos2) . ' | ' . key($mes) }}</th>
+                        @foreach ($concepto as $key => $value)
+                            <td>{{ $value }}</td>
+                        @endforeach
+                    </tr>
+                    @php next($mes); @endphp
+
+                    @endforeach
+
+                    @php next($tablas_calculos2); @endphp
+
+                    @endforeach
+                    </tbody>
+        </table>
 
     </div>
     
